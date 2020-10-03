6a1176802b3ea9384ee27fc4bb01e7b74658306c
Nick <vonottoschtirlitz@gmail.com>
2020-10-02 17:56:14 -0700

    disaster recovery

--- assetcutterheadgear.py
+++ assetcutterheadgear.py
@@ -1,7 +1,7 @@
 import cv2
 from pathlib import Path
 pic_pool = [
-    Path(Path.cwd() / "orig assets/bearskin.png")
+    Path(Path.cwd() / "orig assets/chattrim.png")
 ]
 writepath = Path(Path.cwd() / "1.2"/"Textures"/"Things"/"Apparel"/"Headgear")
 
@@ -33,7 +33,7 @@ for j in range(0,3):
     crop_img = orig_img[0:128*(1), 128*(j):128*(j+1)]
 
 
-    t_writepath = writepath/("bearskin"+"_"+orient_pool[j%3]+".png")
+    t_writepath = writepath/("chat_trim"+"_"+orient_pool[j%3]+".png")
     print(t_writepath)
 
     cv2.imshow("crop", crop_img)
